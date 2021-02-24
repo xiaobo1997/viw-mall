974cb1c598ba5a869944839f12b95a9b45099455
xiaobo1997 <xiaobo97@163.com>
2021-02-24 23:51:47 +0800

    公共模块统一全局异常处理，使用controllerAdvice。使用enum统一异常code方便前后端调试

---
+++ viwmall-product/src/main/java/com/viw/viwmall/product/exception/ViwmallExceptionControllerAdvice.java
@@ -0,0 +1,44 @@
+package com.viw.viwmall.product.exception;
+
+import com.viw.common.exception.BizCodeEnume;
+import com.viw.common.utils.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 统一处理类
+ * @Author: xhb
+ * @Date: 2021/2/24 23:27
+ */
+
+@Slf4j
+//@ControllerAdvice(basePackages = "com.viw.viwmall.product.controller")
+@RestControllerAdvice(basePackages = "com.viw.viwmall.product.controller")
+public class ViwmallExceptionControllerAdvice {
+
+
+    @ExceptionHandler(value= MethodArgumentNotValidException.class)
+    public R handleVaildException(MethodArgumentNotValidException e){
+        log.error("数据校验出现问题{}，异常类型：{}",e.getMessage(),e.getClass());
+        BindingResult bindingResult = e.getBindingResult();
+
+        Map<String,String> errorMap = new HashMap<>();
+        bindingResult.getFieldErrors().forEach((fieldError)->{
+            errorMap.put(fieldError.getField(),fieldError.getDefaultMessage());
+        });
+        return R.error(BizCodeEnume.VAILD_EXCEPTION.getCode(),BizCodeEnume.VAILD_EXCEPTION.getMsg()).put("data",errorMap);
+    }
+
+    @ExceptionHandler(value = Throwable.class)
+    public R handleException(Throwable throwable){
+
+        log.error("错误：",throwable);
+        return R.error(BizCodeEnume.UNKNOW_EXCEPTION.getCode(),BizCodeEnume.UNKNOW_EXCEPTION.getMsg());
+    }
+}
