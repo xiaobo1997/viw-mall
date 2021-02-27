38eec4fdd9a38b4d15afd292f3c3f059793a5096
xiaobo1997 <xiaobo97@163.com>
2021-02-28 01:48:49 +0800

    仓储服务-修改启动服务

--- viwmall-ware/src/main/java/com/viw/viwmall/ware/entity/UndoLogEntity.java
+++ viwmall-ware/src/main/java/com/viw/viwmall/ware/entity/UndoLogEntity.java
@@ -17,44 +17,44 @@ import lombok.Data;
 @Data
 @TableName("undo_log")
 public class UndoLogEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * 
-	 */
-	@TableId
-	private Long id;
-	/**
-	 * 
-	 */
-	private Long branchId;
-	/**
-	 * 
-	 */
-	private String xid;
-	/**
-	 * 
-	 */
-	private String context;
-	/**
-	 * 
-	 */
-	private Longblob rollbackInfo;
-	/**
-	 * 
-	 */
-	private Integer logStatus;
-	/**
-	 * 
-	 */
-	private Date logCreated;
-	/**
-	 * 
-	 */
-	private Date logModified;
-	/**
-	 * 
-	 */
-	private String ext;
+//	private static final long serialVersionUID = 1L;
+//
+//	/**
+//	 *
+//	 */
+//	@TableId
+//	private Long id;
+//	/**
+//	 *
+//	 */
+//	private Long branchId;
+//	/**
+//	 *
+//	 */
+//	private String xid;
+//	/**
+//	 *
+//	 */
+//	private String context;
+//	/**
+//	 *
+//	 */
+//	private Longblob rollbackInfo;
+//	/**
+//	 *
+//	 */
+//	private Integer logStatus;
+//	/**
+//	 *
+//	 */
+//	private Date logCreated;
+//	/**
+//	 *
+//	 */
+//	private Date logModified;
+//	/**
+//	 *
+//	 */
+//	private String ext;
 
 }
