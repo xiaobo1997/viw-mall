d8287010b967bc73b6ca6dbbe2d3d4b7197d3092
xiaobo1997 <xiaobo97@163.com>
2021-03-02 02:14:54 +0800

    ES模块

---
+++ viwmall-search/src/main/java/com/viw/viwmall/search/ViwmallSearchApplication.java
@@ -0,0 +1,13 @@
+package com.viw.viwmall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViwmallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViwmallSearchApplication.class, args);
+    }
+
+}
---
+++ viwmall-search/src/test/java/com/viw/viwmall/search/ViwmallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viw.viwmall.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViwmallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
