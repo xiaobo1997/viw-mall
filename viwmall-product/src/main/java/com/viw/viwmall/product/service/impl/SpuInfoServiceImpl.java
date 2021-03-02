a870a6e39f39dbaaab17bacadb34ccee9d32caac
xiaobo1997 <xiaobo97@163.com>
2021-03-03 00:34:42 +0800

    商品上架service完善

--- viwmall-product/src/main/java/com/viw/viwmall/product/service/impl/SpuInfoServiceImpl.java
+++ viwmall-product/src/main/java/com/viw/viwmall/product/service/impl/SpuInfoServiceImpl.java
@@ -344,7 +344,7 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
             return esModel;
         }).collect(Collectors.toList());
 
-        //TODO 5、将数据发送给es进行保存；gulimall-search；
+        //TODO 5、将数据发送给es进行保存；viwmall-search；
         R r = searchFeignService.productStatusUp(upProducts);
         if(r.getCode() == 0){
             //远程调用成功
