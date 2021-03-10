525b1b0596967bd8555f2401881587e78d59d1a9
xiaobo1997 <xiaobo97@163.com>
2021-03-10 23:45:39 +0800

    1.springboot+rabbitmq 2.创建交换机，创建队列，发送和获取消息，对象的序列化和反序列化传输

---
+++ viwmall-order/src/main/java/com/viw/viwmall/order/config/MyRabbitConfig.java
@@ -0,0 +1,37 @@
+package com.viw.viwmall.order.config;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @Author: xhb
+ * @Email: xiaobo97@163.com
+ * @gitee:https://gitee.com/xiaobo97
+ * @Date: 2021/3/10 23:43
+ * @description: rabbit配置
+ */
+@Configuration
+public class MyRabbitConfig {
+
+
+    /**
+     * 使用JSON序列化机制，进行消息转换
+     */
+    @Bean
+    public MessageConverter messageConverter(){
+
+        return new Jackson2JsonMessageConverter();
+    }
+
+
+}
