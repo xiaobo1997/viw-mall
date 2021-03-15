cacf2af34a99c5abbcc618a29c2f9ba75aed344c
xiaobo1997 <xiaobo97@163.com>
2021-03-16 01:20:45 +0800

    1.sleuth 2.zipkin

--- viwmall-gateway/src/main/java/com/viw/viwmall/gateway/config/SentinelGatewayConfig.java
+++ viwmall-gateway/src/main/java/com/viw/viwmall/gateway/config/SentinelGatewayConfig.java
@@ -1,7 +1,7 @@
 package com.viw.viwmall.gateway.config;
 
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
 import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
-import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.BlockRequestHandler;
 import com.alibaba.fastjson.JSON;
 import com.viw.common.exception.BizCodeEnume;
 import com.viw.common.utils.R;
@@ -15,12 +15,33 @@ import reactor.core.publisher.Mono;
  * @Email: xiaobo97@163.com
  * @gitee:https://gitee.com/xiaobo97
  * @Date: 2021/3/16 0:13
- * @description:
+ * @description: 自定义网关流控返回数据
  */
 
 @Configuration
 public class SentinelGatewayConfig {
 
+    //TODO 响应式编程
+    //GatewayCallbackManager
+    public SentinelGatewayConfig(){
+        GatewayCallbackManager.setBlockHandler(new BlockRequestHandler(){
+            //网关限流了请求，就会调用此回调  Mono Flux
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange exchange, Throwable t) {
 
+                R error = R.error(BizCodeEnume.TOO_MANY_REQUEST.getCode(), BizCodeEnume.TOO_MANY_REQUEST.getMsg());
+                String errJson = JSON.toJSONString(error);
+
+//                Mono<String> aaa = Mono.just("aaa");
+                Mono<ServerResponse> body = ServerResponse.ok().body(Mono.just(errJson), String.class);
+                return body;
+            }
+        });
+
+//        FlowRule flowRule = new FlowRule();
+//        flowRule.setRefResource("gulimall_seckill_route");
+////        flowRule.set
+//        FlowRuleManager.loadRules(Arrays.asList(flowRule));
+    }
 
 }
